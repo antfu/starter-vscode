2dbfcd94edd6ba83d5096905e7451c75887fd043
Anthony Fu <github@antfu.me>
2024-08-16 08:16:10 +0200

    chore: update

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
 import { useLogger } from 'reactive-vscode'
+import { displayName } from './generated/meta'
 
-export const logger = useLogger('ext-name')
+export const logger = useLogger(displayName)
